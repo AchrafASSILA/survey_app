20430c5de82590dcbf9a9da930761a3888f17d7e
AchrafASSILA <achrafassila678@gmail.com>
2022-11-01 16:13:22 +0100

    upload

---
+++ vue/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import "./style.css";
+import store from "./store";
+import router from "./router";
+import "./index.css";
+
+import App from "./App.vue";
+
+createApp(App).use(router).use(store).mount("#app");
---
+++ vue/src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+    state: {
+        user: {
+            data: {
+                name: "achraf",
+            },
+            token: 1,
+        },
+    },
+    getters: {},
+    actions: {},
+    mutations: {},
+    moduls: {},
+});
+
+export default store;
